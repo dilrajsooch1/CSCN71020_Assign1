706980964859453b911b15135b8c3b8eb54b17bd
AnasuyaSesha <abhima@conestogac.on.ca>
2023-01-31 18:15:23 -0500

    commit2

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,47 +3,65 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
 
-	printWelcomeMenu();
+    printWelcomeMenu();
 
-	printOptions();
+    printOptions();
 
-	int inputNum;
+    int inputNum;
 
-	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
+    printf("Enter operation number: ");
+    scanf("%1o", &inputNum);
 
-	switch (inputNum)
-	{
-	case 1:
-		add();
-	}
+    switch (inputNum)
+    {
+        case 1:
+            add();
+            break;
+        case 2:
+            subtract();
+            break;
+    }
 
 }
 
 void printWelcomeMenu() {
-	printf(" **********************\n");
-	printf("**   Welcome to the   **\n");
-	printf("**   BCS Calculator   **\n");
-	printf(" **********************\n");
+    printf(" **********************\n");
+    printf("**   Welcome to the   **\n");
+    printf("**   BCS Calculator   **\n");
+    printf(" **********************\n");
 }
 
 void printOptions() {
-	printf("1. Add\n");
-	printf("2. Subtract\n");
+    printf("1. Add\n");
+    printf("2. Subtract\n");
 }
 
 void add() {
-	double num1, num2, num3, result;
-	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
-	printf("Enter the third value:");
-	scanf_s("%lf", &num3);
-	result = num1 + num2+num3;
-	printf("%lf + %lf = %lf\n", num1, num2, ,num3,result);
+    double num1, num2,num3, result;
+    printf("Enter the first value:");
+    scanf("%lf", &num1);
+    printf("Enter the second value:");
+    scanf("%lf", &num2);
+    printf("Enter the third value:");
+    scanf("%lf", &num3);
+    result = num1 + num2+num3;
+    printf("%lf + %lf+ %lf = %lf\n", num1, num2, num3, result);
+}
+
+void subtract(){
+    double num1, num2,num3, result;
+    printf("Enter the first value:");
+    scanf("%lf", &num1);
+    printf("Enter the second value:");
+    scanf("%lf", &num2);
+    printf("Enter the third value:");
+    scanf("%lf", &num3);
+    result = num1 - num2-num3;
+    printf("%lf - %lf- %lf = %lf\n", num1, num2, num3, result);
+
 }
